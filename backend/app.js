b450e0f1b88e10879e2e2238f645141e88e38d23
HARSH HG GARG <harshgargthapar@gmail.com>
2021-12-08 14:10:39 +0530

    the initial route is working

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,8 @@
 const express =require("express");
 const app =express();
 
-app.use(express.json)
-
-const product=require("./routes/productRoute");
+app.use(express.json())
+//route imports
+const product=require("./routes/productRoute"); 
 app.use("/api/v1",product)
 module.exports=app  
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 const app =require("./app")
 
 const dotenv=require("dotenv");
-dotenv.config({path : "backend/config/dotenv.config.env"})
-app.listen(process.env.PORT,()=>{
-    console.log('Server is working ')
-})
+dotenv.config({path : "backend/config/config.env"})
+const PORT=process.env.PORT;
+app.listen(PORT,()=>{
+    console.log(`Server is working on http://localhost:${process.env.PORT} `)
+});
