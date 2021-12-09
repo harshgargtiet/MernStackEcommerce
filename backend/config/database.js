a2658100083e30b3729b095dfe56b5aac207b930
HARSH HG GARG <harshgargthapar@gmail.com>
2021-12-09 15:59:08 +0530

    added Products CRUD

---
+++ backend/config/database.js
@@ -0,0 +1,17 @@
+const mongoose =require ("mongoose");
+const dotenv=require("dotenv");
+dotenv.config({path : "backend/config/config.env"})
+const URI =process.env.DB_URL ;
+const connectDatabase =()=>{
+    mongoose.connect(URI,{useNewUrlParser:true,useUnifiedTopology:true }).then(
+        (data)=>{
+            console.log(`MongoDB connected with server : ${data.connection.host}`);
+
+        }
+    ).catch((err)=>
+    {
+        console.log(err)
+    })
+}
+
+module.exports=connectDatabase
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 const app =require("./app")
 
 const dotenv=require("dotenv");
+const connectDatabase = require("./config/database")
+
+
 dotenv.config({path : "backend/config/config.env"})
+connectDatabase();
 const PORT=process.env.PORT;
 app.listen(PORT,()=>{
     console.log(`Server is working on http://localhost:${process.env.PORT} `)
