3934a8cc0b9854baef8c5e5bcb4fa7b3119b98ee
HARSH HG GARG <harshgargthapar@gmail.com>
2021-12-12 16:04:58 +0530

    added search filter and pagination feature

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -1,6 +1,7 @@
 const Product = require("../models/productModel");
 const ErrorHandler = require("../utils/errorhandler");
 const catchAsyncErrors=require("../middleware/catchAsyncErrors");
+const ApiFeatures = require("../utils/apifeatures");
 
 
 // Create Product --Admin
@@ -15,8 +16,14 @@ exports.createProduct=catchAsyncErrors(async (req,res,next)=>{
 
 // Get All Products 
 exports.getAllProducts= catchAsyncErrors(async (req,res)=>
-{
-    const products = await  Product.find();
+{    const resultPerPage = 5;
+    const apiFeature =new ApiFeatures( Product.find() , req.query)
+    .search()
+    .filter()
+    .pagination(resultPerPage);                                                                                                                                        ;
+    const  products =await apiFeature.query;
+
+    // const products = await  Product.find(); //passed above 
     res.status(200).json({
         success: true ,
         products,  
