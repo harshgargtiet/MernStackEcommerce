950a24f3137b1892c34b7a2c0c19bde344305f65
HARSH HG GARG <harshgargthapar@gmail.com>
2021-12-25 15:37:19 +0530

    added order model and controller

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,5 +1,5 @@
 const express =require("express");
-const { getAllProducts ,createProduct, updateProduct, deleteProduct, getProductDetails} = require("../controller/productController");
+const { getAllProducts ,createProduct, updateProduct, deleteProduct, getProductDetails, createProductReview, getProductReviews, deleteReview} = require("../controller/productController");
 const router= express.Router();
 const {isAuthenticatedUser,authorizeRoles} =require("../middleware/auth");
 
@@ -7,5 +7,10 @@ router.route("/products").get(getAllProducts);
 router.route("/admin/product/new").post(isAuthenticatedUser,authorizeRoles("admin"),createProduct);
 router.route("/admin/product/:id").put(isAuthenticatedUser,authorizeRoles("admin"),updateProduct).delete(isAuthenticatedUser,authorizeRoles("admin"),deleteProduct).get(getProductDetails) ;
 router.route("/product/:id").get(getProductDetails) ;
+router.route("/review").put(isAuthenticatedUser,createProductReview);
+router
+  .route("/reviews")
+  .get(getProductReviews)
+  .delete(isAuthenticatedUser, deleteReview);
 
 module.exports=router
